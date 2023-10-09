b7f0b15b9f94ab8c98d48b20355c7c697d0541b4
github_username <you@example.com>
2023-10-09 18:24:25 +0700

    Nearly finish

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,14 @@ const user = {
         const query = await db.query('SELECT * FROM users WHERE userid = $1', [userId]);
         return query.rows[0];
     },
+    getCart: async function (userId) {
+        const query = await db.query('SELECT * FROM carts C LEFT JOIN products P ON P.productid = C.product_id WHERE user_id = $1 ORDER BY C.cartid', [userId]);
+        return query.rows;
+    },
+    getProductPrice: async function (productId) {
+        const query = await db.query('SELECT discountprice FROM products WHERE productid = $1', [productId]);
+        return query.rows[0];
+    },
     getFavorites: async function (userId) {
         const query = await db.query('SELECT * FROM favorites F LEFT JOIN products P ON F.product_id = P.productid WHERE user_id = $1', [userId]);
         return query.rows;
@@ -28,6 +36,61 @@ const user = {
         const query = await db.query('UPDATE users SET phone = $1, password = $2, fullname = $3, address = $4 WHERE userid = $5;', [user.phone, user.password, user.fullname, user.address, userId]);
         if (query.rowCount > 0) return true;
         else return false;
+    },
+    updateImgUser: async function (image, userId) {
+        const query = await db.query('UPDATE users SET image = $1 WHERE userid = $2;', [image, userId]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    addToCart: async function (userId, productId, quantity, totalPrice) {
+        const query = await db.query('INSERT INTO carts (user_id, product_id, quantity, totalprice) VALUES ($1,$2,$3,$4);', [userId, productId, quantity, totalPrice]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    changeQuantityCart: async function (quantity, totalPrice, cartId) {
+        const query = await db.query('UPDATE carts SET quantity = $1, totalPrice = $2 WHERE cartid = $3', [quantity, totalPrice, cartId]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    newOrder: async function (userId, dateorder, status, note) {
+        const query = await db.query('INSERT INTO orders (user_id, dateorder, status, note) VALUES ($1,$2,$3,$4);', [userId, dateorder, status, note]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    addToDetails: async function (orderId, productId, quantity, totalPrice) {
+        const query = await db.query('INSERT INTO details (order_id, product_id, quantity, totalprice) VALUES ($1,$2,$3,$4);', [orderId, productId, quantity, totalPrice]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    getOrderId: async function (userId) {
+        const query = await db.query('SELECT * FROM orders WHERE user_id = $1;', [userId]);
+        return query.rows[0];
+    },
+    getOrderIdUnpaid: async function (userId) {
+        const query = await db.query('SELECT * FROM orders WHERE user_id = $1 AND status = false;', [userId]);
+        return query.rows[0];
+    },
+    deletePurchasedProduct: async function (cartId) {
+        const query = await db.query('DELETE FROM carts WHERE cartid = $1;', [cartId]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    getDetails: async function (orderId) {
+        const query = await db.query('SELECT * FROM details C LEFT JOIN products P ON P.productid = C.product_id WHERE C.order_id = $1', [orderId]);
+        return query.rows;
+    },
+    updateOrder: async function (orderId) {
+        const query = await db.query('UPDATE orders SET status = true WHERE orderid = $1', [orderId]);
+        if (query.rowCount > 0) return true;
+        else return false;
+    },
+    getOrders: async function (userId) {
+        const query = await db.query('SELECT * FROM orders WHERE user_id = $1', [userId]);
+        return query.rows;
+    },
+    getOrder: async function (orderId) {
+        const query = await db.query('SELECT * FROM orders WHERE orderid = $1', [orderId]);
+        return query.rows[0];
     },
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const model = require('../models/userModel'); 
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 /* GET users listing. */
 router.get('/profile', async function(req, res) {
@@ -8,11 +11,13 @@ router.get('/profile', async function(req, res) {
   try {
     const user = await model.getUser(userId);
     const favorites = await model.getFavorites(userId);
+    const orders = await model.getOrders(userId);
 
     res.render('user/profile', { 
         title: 'Trang cá nhân',
         user: user,
-        favorites: favorites
+        favorites: favorites,
+        orders: orders
     });
   } catch (error) {
     res.redirect('error');
@@ -20,23 +25,6 @@ router.get('/profile', async function(req, res) {
 });
 
 router.post('/profile', async function(req, res) {
-  const userId = 1;
-  const { phone, password, fullname, address } = req.body;
-  try {
-    const user = {
-      phone: phone,
-      password: password,
-      fullname: fullname,
-      address: address
-    }
-    const updateUser = model.updateUser(user, userId);
-    res.redirect('/user/profile');
-  } catch (error) {
-    res.redirect('/error');
-  }
-});
-
-router.post('/profile/image', async function(req, res) {
   const userId = 1;
   const { phone, password, fullname, address } = req.body;
   try {
@@ -83,6 +71,217 @@ router.get('/favorite/delete/:favoriteid', async function(req, res) {
   } catch (error) {
     console.error(error);
     res.redirect('/error');
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './public/images/user');
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "_" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.post('/profile/image', upload.single('image'), async function(req, res) {
+  try {
+    const userId = 1; // Replace with the actual user ID
+
+    // Retrieve the user's image filename
+    const user = await model.getUser(userId);
+    // Check if the user has an existing image
+    if (user.image || user.image != null) {
+      // Construct the path to the existing image
+      const imagePath = `./public/images/user/${user.image}`;
+
+      // Remove the existing image file
+      fs.unlink(imagePath, (err) => {
+        if (err) {
+          res.redirect('/error');
+          return;
+        }
+      });
+    }
+
+    // Check if a new image was uploaded
+    if (req.file && req.file.filename) {
+      // Update the user's image filename in the database
+      await model.updateImgUser(req.file.filename, userId);
+      console.log("image to db: " + req.file.filename);
+    }
+
+    res.redirect('/user/profile');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/error');
+  }
+});
+
+router.post('/handleorder', async function(req, res) {
+  const command = req.body.command;
+  const userId = 1;
+  const productId = req.body.productid;
+  try {
+    const quantity = req.body.quantity;
+    const price = req.body.discountprice;
+    const totalPrice = quantity * price;
+
+    await model.addToCart(userId, productId, quantity, totalPrice);
+    if (command == 'cart')
+    {
+      res.redirect('/product');
+    }
+    else
+    {
+      res.redirect('/user/cart');
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect('error');
+  }
+});
+
+router.get('/cart', async function(req, res) {
+  const userId = 1;
+  let tmpPrice = 0;
+  let totalPrice = 0;
+  try {
+    const user = await model.getUser(userId);
+    const cart = await model.getCart(userId);
+
+    for (let i = 0; i < cart.length; i++) {
+      tmpPrice += parseFloat(cart[i].totalprice); // Parse the value as a float and add it to the total
+    }
+    totalPrice = tmpPrice + 15000;
+
+    res.render('user/cart', { 
+        title: 'Giỏ hàng',
+        user: user,
+        cart: cart,
+        tmpPrice: tmpPrice,
+        totalPrice: totalPrice
+    });
+  } catch (error) {
+    res.redirect('error');
+  }
+});
+
+router.post('/cart', async function(req, res) {
+  const quantity = req.body.quantity;
+  const productId = req.body.productid;
+  const cartId = req.body.cartid;
+  try {
+    const productPrice = await model.getProductPrice(productId);
+    let totalPrice = productPrice.discountprice * quantity;
+    console.log(totalPrice);
+    console.log(cartId);  
+    await model.changeQuantityCart(quantity, totalPrice, cartId);
+
+    res.redirect('/user/cart');
+  } catch (error) {
+    res.redirect('error');
+  }
+});
+
+router.post('/checkout', async function(req, res) {
+  const note = req.body.note;
+  const userId = 1;
+  try {
+    const cart = await model.getCart(userId);
+
+    const dateorder = new Date();
+    const status = false;
+    await model.newOrder(userId, dateorder, status, note);
+
+    const orderId = await model.getOrderId(userId);
+
+    for (let i = 0; i < cart.length; i++) {
+      await model.addToDetails(orderId.orderid, cart[i].product_id, cart[i].quantity, cart[i].totalprice);
+      await model.deletePurchasedProduct(cart[i].cartid);
+    }
+
+    res.redirect('/user/payment');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/error');
+  }
+});
+
+router.get('/payment', async function(req, res) {
+  const userId = 1;
+  let totalPrice = 0;
+  let note = "";
+  try {
+    let details = {};
+    const order = await model.getOrderIdUnpaid(userId);
+    const user = await model.getUser(userId);
+    if (order != null || order != undefined) {
+      details = await model.getDetails(order.orderid);
+      note = order.note;
+    }
+    else
+    {
+      res.redirect('/product');
+    }
+
+    for (let i = 0; i < details.length; i++) {
+      totalPrice += parseFloat(details[i].totalprice); // Parse the value as a float and add it to the total
+    }
+
+    res.render('user/payment', { 
+        title: 'Thanh toán',
+        user: user,
+        details: details,
+        totalPrice: totalPrice + 15000,
+        note: note
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('error');
+  }
+});
+
+router.get('/payment/:orderid', async function(req, res) {
+  const orderId = req.params.orderid;
+  const userId = 1;
+  let totalPrice = 0;
+  let note = "";
+  try {
+    let details = {};
+    const order = await model.getOrder(orderId);
+    const user = await model.getUser(userId);
+    details = await model.getDetails(order.orderid);
+    note = order.note;
+
+    for (let i = 0; i < details.length; i++) {
+      totalPrice += parseFloat(details[i].totalprice); // Parse the value as a float and add it to the total
+    }
+
+    res.render('user/payment', { 
+        title: 'Thanh toán',
+        user: user,
+        details: details,
+        totalPrice: totalPrice + 15000,
+        note: note,
+        status: order.status
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('error');
+  }
+});
+
+router.get('/paid', async function(req, res) {
+  const userId = 1;
+  try {
+    const order = await model.getOrderIdUnpaid(userId);
+    await model.updateOrder(order.orderid);
+
+    res.redirect('/user/profile');
+  } catch (error) {
+    console.log(error);
+    res.redirect('error');
   }
 });
 
